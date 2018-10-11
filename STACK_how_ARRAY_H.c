0a8425b4d53fe1d3f449cb4e02829a9d7ea2c801
NNikitaB <42946181+NNikitaB@users.noreply.github.com>
2018-10-11 22:48:40 +0300

    Add files via upload

---
+++ STACK_how_ARRAY_H.c
@@ -0,0 +1,69 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define STACKSIZE 200
+#define STACKOK 0
+#define STACKEMPLY 1
+#define STACKFULL 2
+#define STACKNOTMEM 3
+extern StackError;
+typedef struct {void * buf; size_t i; } stack;
+
+void  stack_init(stack * s,size_t size_slot_arr );
+void stackput(stack * s,void * basetype,size_t size_slot_arr);
+void stackout(stack * s,void * basetype,size_t size_slot_arr);
+int stackemply(stack * s);
+int stackfull(stack * s);
+void error_print_stack(int a);
+
+
+
+
+
+void error_print_stack(int a)
+{
+    switch(a)
+    {
+        case STACKOK: fputs("All-OK",stdout);break;
+        case STACKEMPLY: fputs("STACK-EMLY",stdout);break;
+        case STACKFULL: fputs("STACK-FULL",stdout);break;
+        case STACKNOTMEM: fputs("STACK-NOT MEMORY IN DINAMIC MEM",stdout);break;
+        default: fputs("Unknown-error",stdout);break;
+    }
+}
+
+void  stack_init(stack * s,size_t size_slot_arr )
+{
+    void *stk=malloc(size_slot_arr*STACKSIZE+sizeof(stack));
+    s=(stack *)stk;
+    s->buf=stk;
+    s->i=0;
+    if(stk==NULL) StackError=STACKNOTMEM;
+}
+void stackput(stack * s,void * basetype,size_t size_slot_arr)
+{
+     if(s->buf!=NULL && basetype!=NULL )
+        if(!stackfull(s))
+                 {memmove(s->buf+s->i*size_slot_arr,basetype,size_slot_arr);  ++s->i;}
+        else if(StackError==STACKOK) StackError=STACKFULL ;
+            else {error_print_stack(StackError);StackError=STACKFULL;}
+     else StackError=55;
+}
+void stackout(stack * s,void * basetype,size_t size_slot_arr)
+{
+     if(s->buf!=NULL && basetype!=NULL )
+        if(!stackemply(s))
+                 {--s->i;memmove(basetype,s->buf+s->i*size_slot_arr,size_slot_arr);}
+        else if(StackError==STACKOK) StackError=STACKEMPLY ;
+            else {error_print_stack(StackError);StackError=STACKEMPLY;}
+     else StackError=55;
+}
+int stackemply(stack * s)
+{
+   return s->i==0?1:0;
+}
+int stackfull(stack * s)
+{
+   return s->i==STACKSIZE?1:0;
+}
